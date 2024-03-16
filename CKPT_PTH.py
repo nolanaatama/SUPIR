b0456d0a71de068594d23eb246a502ff043dfabf
Nolan Aatama <119261289+nolanaatama@users.noreply.github.com>
2024-03-16 13:27:36 +0700

    Add files via upload

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,4 @@
-LLAVA_CLIP_PATH = 'workspace/SUPIR/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = 'workspace/SUPIR/llava-v1.5-13b'
-SDXL_CLIP1_PATH = 'workspace/SUPIR/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = 'workspace/SUPIR/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
+LLAVA_CLIP_PATH = '/workspace/SUPIR/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = '/workspace/SUPIR/llava-v1.5-13b'
+SDXL_CLIP1_PATH = '/workspace/SUPIR/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = '/workspace/SUPIR/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
